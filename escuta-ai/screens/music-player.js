c7e48c1de321ca21f0f884066396ec7debb05002
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-04-26 11:16:34 -0300

    Started adding the real music player

--- escuta-ai/screens/music-player.js
+++ escuta-ai/screens/music-player.js
@@ -1,28 +1,73 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+import data from "./data.json";
 
 const MusicPlayer = () => {
+  const audio_player = document.querySelector("#audio-player");
+
+  const playlists = data;
+
+  let current_song = 0;
+  let current_playlist = 0;
+
+  const load_song = () => {
+    const playlists = data;
+    const song = playlists[current_playlist].musics[current_song];
+
+    const cover_element = document.querySelector("#cover");
+    const title_element = document.querySelector("#title");
+
+    audio_player.src = song.src;
+    cover_element.src = song.cover;
+    title_element.textContent = song.name;
+  }
+
+  const play_music = () => {
+    if (audio_player.paused) {
+        audio_player.play();
+        play_button.firstElementChild.src = "./assets/icons/pause.png";
+    } else {
+        audio_player.pause();
+        play_button.firstElementChild.src = "./assets/icons/play-buttton.png";
+    }
+  }
+
+  const next_music = () => {
+    current_song = (current_song + 1) % playlists[current_playlist].musics.length;
+    load_song();
+  }
+
+  const prev_music = () => {
+    current_song = (current_song - 1 + playlists[current_playlist].musics.length) % playlists[current_playlist].musics.length;
+    load_song();
+  }
+
   return (
     <View style={styles.container}>
         <Image 
           source={{ uri: "../assets/covers/guilty-gear-cover.jpg" }}
           style={{ width: 300, height: 300, borderRadius: 10, marginBottom: 25 }}
         />
-        <View style={styles.line}></View>
+        <View style={styles.audioPlayer}>
+          <audio id="audio-player" controls>
+            <source src="../assets/songs/playlist1/Smell-of-the-Game.mp3" type="audio-mpeg"/>
+          </audio>
+        </View>
         <View style={styles.mediaController}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={prev_music}>
             <Image 
               source={{ uri: "../assets/icons/back-arrow.png" }}
               style={styles.buttonImage}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={play_music}>
             <Image 
               source={{ uri: "../assets/icons/play-buttton.png" }}
               style={styles.buttonImage}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={next_music}>
             <Image 
               source={{ uri: "../assets/icons/right-arrow.png" }}
               style={styles.buttonImage}
@@ -53,12 +98,8 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
-  line: {
-    backgroundColor: "#595959",
-    borderRadius: 15,
-    height: 5,
-    width: "75%",
-    marginBottom: 25,
+  audioPlayer: {
+    marginBottom: 25
   }
 });
 
